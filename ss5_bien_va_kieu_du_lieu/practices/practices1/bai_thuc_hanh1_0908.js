e3bfed26f2ae81c4ce5067793237222c4916fca4
xuany1611 <xuany1611@gmail.com>
2022-09-07 17:15:45 +0700

    ss5 biến và kiểu dữ liệu toán tử

---
+++ ss5_bien_va_kieu_du_lieu/practices/practices1/bai_thuc_hanh1_0908.js
@@ -0,0 +1,12 @@
+var i =10;
+var f =20.5;
+var b =true;
+var s m Hà Nội;
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)

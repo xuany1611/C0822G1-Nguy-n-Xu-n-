5e3c6ab70302b987fc0d907e51e9b92ca59bf837
xuany1611 <xuany1611@gmail.com>
2022-09-08 17:21:45 +0700

    bài tập 0908

---
+++ ss5_bien_va_kieu_du_lieu/practices/practices1/bai_thuc_hanh2_0908.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("enter the width");
+    inputWidth = prompt ("enter the heihgt")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
